a20a64fdba7aa51099a87577cb2a48253d27b029
TAMAGO55123 <tamago.55123@gmail.com>
2025-02-17 18:34:10 +0900

    [add]admin_bot_stopでメッセージを送信してから停止するように変更

--- cogs/admin.py
+++ cogs/admin.py
@@ -25,10 +25,9 @@ class AdminCog(commands.Cog):
     @app_commands.command(name="admin_bot_stop", description="Botを停止させます。(管理者のみ)")
     async def admin_bot_stop(self, interaction:discord.Interaction, ok:bool):
         if ok & check_admin(interaction.user.id):
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.unknown))
+            await interaction.response.send_message(embed=discord.Embed(title="Botが停止しました。",description="再開するにはサーバーで`sudo systemctl start tbot`をする必要があります。"), ephemeral=True)
             sys.exit()
-    
-
-    
 
 async def setup(bot):
     await bot.add_cog(AdminCog(bot))
